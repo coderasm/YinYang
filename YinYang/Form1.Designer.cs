e5f3944b24217f831da33afd774db6f105e27b39
pkrueger <philip.krueger@caci.com>
2019-04-16 23:42:06 -0700

    first push

--- YinYang/Form1.Designer.cs
+++ YinYang/Form1.Designer.cs
@@ -28,10 +28,16 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Form1";
+      this.SuspendLayout();
+      // 
+      // Form1
+      // 
+      this.ClientSize = new System.Drawing.Size(284, 261);
+      this.Name = "Form1";
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.onPaint);
+      this.Resize += new System.EventHandler(this.onResize);
+      this.ResumeLayout(false);
+
     }
 
     #endregion
--- YinYang/Form1.cs
+++ YinYang/Form1.cs
@@ -1,20 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace YinYang
 {
   public partial class Form1 : Form
   {
+    private int largeDiameter = 200;
     public Form1()
     {
       InitializeComponent();
+      ClientSize = new Size(largeDiameter, largeDiameter);
+      MinimumSize = ClientSize;
+    }
+
+    private void onPaint(object sender, PaintEventArgs e)
+    {
+      var mediumDiameter = largeDiameter / 2;
+      var smallDiameter = mediumDiameter / 3;
+      var pen = new Pen(Color.White);
+      var brush = new SolidBrush(Color.White);
+      var rect = new Rectangle(0, 0, largeDiameter, largeDiameter);
+
+      //color background
+      e.Graphics.DrawRectangle(pen, rect);
+      e.Graphics.FillRectangle(brush, rect);
+      pen.Color = Color.Black;
+      brush.Color = Color.Black;
+
+      //Largest Parent Circle
+      //Draw left
+      e.Graphics.DrawPie(pen, rect, 270, 180);
+      e.Graphics.FillPie(brush, rect, 270, -180);
+      //Draw right
+      var penWidth = pen.Width;
+      e.Graphics.DrawPie(pen, rect, 90, 180);
+
+      //Draw top black circle
+      var upperX = (largeDiameter - mediumDiameter) / 2;
+      rect = new Rectangle(upperX, 0, mediumDiameter, mediumDiameter);
+      e.Graphics.DrawEllipse(pen, rect);
+      e.Graphics.FillEllipse(brush, rect);
+
+      //Draw bottom white circle
+      var bottomY = largeDiameter - mediumDiameter;
+      rect = new Rectangle(upperX, bottomY, mediumDiameter, mediumDiameter);
+      pen.Color = Color.White;
+      e.Graphics.DrawEllipse(pen, rect);
+      brush.Color = Color.White;
+      e.Graphics.FillEllipse(brush, rect);
+
+      //Draw top white circle
+      var topX = largeDiameter - mediumDiameter - smallDiameter / 2;
+      var topY = (mediumDiameter - smallDiameter) / 2;
+      rect = new Rectangle(topX, topY, smallDiameter, smallDiameter);
+      e.Graphics.DrawEllipse(pen, rect);
+      brush.Color = Color.White;
+      e.Graphics.FillEllipse(brush, rect);
+
+      //Draw bottom black circle
+      var lowerY = (largeDiameter - mediumDiameter) + (mediumDiameter - smallDiameter) / 2;
+      rect = new Rectangle(topX, lowerY, smallDiameter, smallDiameter);
+      brush.Color = Color.Black;
+      pen.Color = Color.Black;
+      e.Graphics.DrawEllipse(pen, rect);
+      e.Graphics.FillEllipse(brush, rect);
+    }
+
+    private void onResize(object sender, System.EventArgs e)
+    {
+      //Resize drawing to new window size
+      largeDiameter = ClientSize.Height > ClientSize.Width ? ClientSize.Height : ClientSize.Width;
+      ClientSize = new Size(largeDiameter, largeDiameter);
+      Invalidate();
     }
   }
 }
